328f78cf3eac7f27287e9930cb57577c7e33d1fa
vian <707820685@qq.com>
2020-02-14 19:18:28 +0800

    构建环境

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = require('./router')
+
+const app = express()
+
+app.use('/', router)
+
+const server = app.listen('5000', function() {
+    const { port } = server.address();
+    console.log(`http服务已在${port}端口启动...`);
+})
